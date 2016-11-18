7fe89882ffab55c26f5d33436855260293a4db5c
thenameisangel <y.lo@utexas.edu>
2016-11-17 22:24:32 -0600

    Added user login and registration pages with Materialize form components

--- php/login.php
+++ php/login.php
@@ -1,7 +1,22 @@
 <?php
+/*
+if (isset($_POST['login'])) {
+	loginResultPage();
+} else if (!isset($_COOKIE['SESSION_ID'])) {
+	loginPage();
+} else if (isset($_POST['logout'])) {
+	logout();
+} else if (isset($_GET['registerPage'])) {
+	registerPage();
+} else {
+	loginSucceededPage();
+}
+*/
 
 if (isset($_POST['login'])) {
 	loginResultPage();
+} else if (isset($_GET['registerPage'])) {
+	registerPage();
 } else if (!isset($_COOKIE['SESSION_ID'])) {
 	loginPage();
 } else if (isset($_POST['logout'])) {
@@ -9,7 +24,111 @@ if (isset($_POST['login'])) {
 } else {
 	loginSucceededPage();
 }
+
+$script = $_SERVER['PHP_SELF'];
+
 ################################################################
+
+function registerPage() {
+
+$script = $_SERVER['PHP_SELF'];
+
+print <<<REGISTER
+<html>
+<head>
+<title>Register</title>
+<!-- import google fonts -->
+<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<!-- import materialize.css -->
+<link type="text/css" rel="stylesheet" href="../css/materialize.min.css"  media="screen,projection"/>
+<!-- optimize browser for mobile -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+
+<div class="container">
+
+<h1 align="center">Register for an Account</h1>
+
+<div class="row">
+    <form method="post" action="$script" class="col s12">
+      <div class="row">
+        <div class="input-field col s6">
+          <input name="firstName" type="text" class="validate">
+          <label for="first_name">First Name</label>
+        </div>
+        <div class="input-field col s6">
+          <input name="lastName" type="text" class="validate">
+          <label for="last_name">Last Name</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input name="password_1" type="password" class="validate">
+          <label for="password">Password</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input name="password" type="password" class="validate">
+          <label for="password">Confirm Password</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input name="email" type="email" class="validate">
+          <label for="email">Email</label>
+        </div>
+      </div>
+      <button class="btn waves-effect waves-light" type="submit" name="register">Register
+         <i class="material-icons right">send</i>
+       </button>
+    </form>
+  </div>
+</div>
+
+<!-- import materialize js files -->
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="../js/materialize.min.js"></script>
+
+</body>
+
+REGISTER;
+}
+
+function register() {
+    
+    //read in valid credentials from file
+    $users = file_get_contents('passwd.txt');
+    $fh = fopen('passwd.txt','r');
+    
+    //retrieve user login attempt credentials
+    $username = trim($_POST["username"]);
+    $password = trim($_POST['password']);
+    
+    //check if user entered an empty string
+    if ($username == "" || $password == "") {
+        registerPage();
+    }
+
+    //check if user entered an existing username
+    if (strpos($users, "\n".$username.":") == true){
+        print "That username already exists. Please try again.";
+    }
+
+    //concatenate user input and add to file
+    $info = $username.":".$password;
+    fclose($fh);
+    if (strpos($users, $info)!== false) {
+        loginResultPage();
+    } else {
+        $fh = fopen('passwd.txt','a');
+        fwrite($fh, $info."\n");
+        loginResultPage();
+    }
+
+
+}
 
 
 function loginPage() {
@@ -19,30 +138,49 @@ print <<<LOGIN
 <html>
 <header>
 <title>Login Page</title>
+<!-- import google fonts -->
+<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<!-- import materialize.css -->
+<link type="text/css" rel="stylesheet" href="../css/materialize.min.css"  media="screen,projection"/>
+<!-- optimize browser for mobile -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </header>
 <body>
-	<form method="post" action="$script">
-	<table border=0 align="center">
-		<tr>
-			<td align="center">
-				Username: <br/>
-				<input type="text" name="username"/>
-			</td>
-		</tr>
-		<tr>
-			<td align="center">
-				Password: <br/>
-				<input type="password" name="password"/>
-			</td>
-		</tr>
-		<tr>
-			<td align="center">
-				<input type="submit" name="login" value="Login"/>
-				<input type="reset" value="Clear"/>
-			</td>
-		</tr>
-	</table>
-	</form>
+<div class="container">
+
+<h1> Login </h1>
+
+<div class="row">
+    <form method="post" action="$script" class="col s12">
+      <div class="row">
+        <div class="input-field col s6">
+          <input name="userName" type="text" class="validate">
+          <label for="username">Username</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input name="password" type="password" class="validate">
+          <label for="password">Password</label>
+        </div>
+      </div>
+      <button class="btn waves-effect waves-light" type="submit" name="login">Login
+         <i class="material-icons right">send</i>
+       </button>
+      <button class="btn waves-effect waves-light" type="reset">Clear
+         <i class="material-icons right">send</i>
+       </button>
+    </form>
+  </div>
+</form>	
+
+<form method="get" action="$script">
+	<a href="?registerPage">Register for an account</a>
+</form>
+
+<!-- import materialize js files -->
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="../js/materialize.min.js"></script>
 </body>
 </html>
 
@@ -60,7 +198,7 @@ if ($_POST['username'] != 'guest' || $_POST['password'] != 'welcome') {
 } else {
 	$sessionId = session_start();
 	$loginTime = time();
-	setcookie('SESSION_ID', $sessionId, time() + 60, '/');
+	setcookie('SESSION_ID', $sessionId, time() + 60, '/'); 
 	setcookie('LOGIN_TIME', $loginTime, time() + 60, '/');
 	loginSucceededPage();
 }
